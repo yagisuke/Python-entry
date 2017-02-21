4a31793a78fc535dc96449df0ae1ad36a2e9439a
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-02-22 01:27:18 +0900

    Pythonの教科書 -p.41

---
+++ no3-inch-to-cm.py
@@ -0,0 +1,5 @@
+# インチをセンチメートルに変換
+per_inch = 2.54
+inch = 8
+print('8inchのタブレットは' + str(inch * per_inch) + 'cm') # 8inchのタブレットは20.32cm
+print('8inchのタブレットは', inch * per_inch, 'cm') # 8inchのタブレットは 20.32 cm
