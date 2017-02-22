460840c68112a72af549690438fbf814bcb2cbbe
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-02-23 02:06:53 +0900

    Pythonの教科書 -p.49

--- no6-string-format.py
+++ no6-string-format.py
@@ -4,7 +4,7 @@ age = "28"
 str = "{0}は{1}歳になりました.".format(name, age)
 print(str) # Carlosは28歳になりました。
 
-# formatで名前月引数を使う
+# formatで名前付き引数を使う方法
 print("私は{sports_name}が好きです.".format(sports_name="Soccer")) # 私はSoccerが好きです
 format = "私は{pet}を飼っています."
 print(format.format(pet="Dog")) # 私はDogを飼っています
